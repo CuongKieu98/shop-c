ed2a3a32bd9f0e7b7f8171cb9a39e19802454c33
Cuong98 <cuongkieu2898@gmail.com>
2022-07-13 09:09:27 +0700

    create base

---
+++ src/components/GlobalStyle/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Stack,Button } from "react-bootstrap";
+import "./GlobalStyle.scss";
+
+function GlobalStyle({children}) {
+  return (
+    children
+  );
+}
+
+export default GlobalStyle;
